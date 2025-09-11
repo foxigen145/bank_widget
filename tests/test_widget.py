6f7b4fa339e10412fce409152d4cbeb9b3294bab
Oleg <foxigen145@gmail.com>
2025-09-12 02:59:50 +0300

    Добавил тесты для widget

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -5,8 +5,10 @@ from src.widget import mask_account_card
 def test_mask_account_card_card() -> None:
     assert mask_account_card("Visa Platinum 7000792289066361") == "Visa Platinum 7000 79** **** 6361"
 
+
 def test_mask_account_card_account() -> None:
     assert mask_account_card("Счет 73654108430135874305") == "Счет **4305"
+
 
 def test_get_date() -> None:
     assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
